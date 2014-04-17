537f787189ad6328eadcb058878bbc3406323152
bjtuweb-12283023 <12283023@bjtu.edu.cn>
2014-04-17 17:11:33 +0800

    assignment3

---
+++ insert.php
@@ -0,0 +1,21 @@
+﻿<?php
+$con = mysql_connect("localhost","peter","abc123");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("people", $con);
+
+$sql="yanzheng(name,password)
+VALUES
+('$_POST[name]','$_POST[password]')";
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+
+mysql_close($con)
+?>
